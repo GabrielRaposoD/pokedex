763ae773730a05bbd05cf8e28468610654c80d5a
Gabriel <gabriel.robertor@gmail.com>
2021-09-19 12:18:15 -0300

    layla: finishes pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,5 +1,6 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 import cs from 'clsx';
+import { usePagination } from 'hooks/usePagination';
 
 interface PaginationProps {
   total: number;
@@ -14,21 +15,33 @@ const Pagination: React.FC<PaginationProps> = ({
   total,
   itemsPerPage,
 }) => {
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount: total,
+    siblingCount: 2,
+    pageSize: itemsPerPage,
+  });
+
   const PAGE_TOTAL = Math.ceil(total / itemsPerPage);
-  const pages = [...new Array(PAGE_TOTAL)].map((_, i) => i + 1);
-  console.log(currentPage);
+
   return (
     <div className='sm:px-6 flex items-center justify-between px-4 py-3 bg-white border-t border-gray-200'>
       <div className='sm:hidden flex justify-between flex-1'>
         <button
           type='button'
           className='hover:bg-gray-50 relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md'
+          onClick={() =>
+            currentPage > 1 ? setCurrentPage(currentPage - 1) : null
+          }
         >
           Previous
         </button>
         <button
           type='button'
           className='hover:bg-gray-50 relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md'
+          onClick={() => {
+            currentPage < PAGE_TOTAL ? setCurrentPage(currentPage + 1) : null;
+          }}
         >
           Next
         </button>
@@ -64,48 +77,26 @@ const Pagination: React.FC<PaginationProps> = ({
               <span className='sr-only'>Previous</span>
               <ChevronLeftIcon className='w-5 h-5' aria-hidden='true' />
             </button>
-            {pages
-              .slice(
-                currentPage - 1 <= PAGE_TOTAL - 9
-                  ? currentPage > 5
-                    ? currentPage - 1
-                    : 0
-                  : PAGE_TOTAL - 9,
-                currentPage > 5 ? currentPage + 8 : 9
-              )
-              .map((v, i) => {
-                const page =
-                  i === 0
-                    ? pages[i]
-                    : i === 1
-                    ? currentPage <= 5
-                      ? 2
-                      : '...'
-                    : i === 7
-                    ? currentPage >= PAGE_TOTAL - 5
-                      ? PAGE_TOTAL - 1
-                      : '...'
-                    : i === 8
-                    ? pages[PAGE_TOTAL - 1]
-                    : pages[v - i];
-                return (
-                  <li
-                    onClick={() => {
-                      setCurrentPage(page as number);
-                    }}
-                    key={i}
-                    aria-current='page'
-                    className={cs({
-                      'hover:bg-gray-50 relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300':
-                        page !== currentPage,
-                      'bg-indigo-50 relative z-10 inline-flex items-center px-4 py-2 text-sm font-medium text-indigo-600 border border-indigo-500':
-                        page === currentPage,
-                    })}
-                  >
-                    {page}
-                  </li>
-                );
-              })}
+            {paginationRange.map((page, i) => {
+              return (
+                <li
+                  onClick={() => {
+                    setCurrentPage(page as number);
+                  }}
+                  key={i}
+                  aria-current='page'
+                  className={cs({
+                    'hover:bg-gray-50 relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-pointer':
+                      page !== currentPage,
+                    'bg-indigo-50 relative z-10 inline-flex items-center px-4 py-2 text-sm font-medium text-indigo-600 border border-indigo-500 pointer-events-none':
+                      page === currentPage,
+                    'pointer-events-none': page === '...',
+                  })}
+                >
+                  {page}
+                </li>
+              );
+            })}
             <button
               type='button'
               className='rounded-r-md hover:bg-gray-50 relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300'
