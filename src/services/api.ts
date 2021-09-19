ffebd05b4c11a227c62060e7402303520ef99a72
Gabriel <gabriel.robertor@gmail.com>
2021-09-19 14:56:06 -0300

    now uses env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://pokeapi.co/api/v2',
+  baseURL: process.env.NEXT_PUBLIC_POKEAPI_API_URL,
 });
 
-const baseClient = axios.create({ baseURL: 'http://localhost:3000/api' });
+const baseClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+});
 
 export { apiClient, baseClient };
